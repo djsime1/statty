dffe2601f468b0ecbf68205e7d0b5985e732aeb7
djsime1 <dj@dj.je>
2021-05-24 00:24:52 -0500

    Inital modifications for Railway

--- lib/env/index.ts
+++ lib/env/index.ts
@@ -10,12 +10,9 @@ export const IS_SERVER = typeof window === 'undefined'
 // #endregion
 
 // #region Application
-export const CONFIG_PATH = registerString('CONFIG_PATH')
+export const CONFIG_PATH = registerString('CONFIG_PATH') ?? "./config.json"
 // #endregion
 
 // #region Redis
-export const REDIS_HOST = registerString('REDIS_HOST') ?? (IS_DEV ? 'localhost' : 'redis')
-export const REDIS_PORT = registerInt('REDIS_PORT') ?? 6379
-export const REDIS_PASS = registerString('REDIS_PASS')
-export const REDIS_DB_BASE = registerInt('REDIS_DB_BASE') ?? 0
+export const REDIS_URL = registerString('REDIS_URL')
 // #endregion
--- lib/redis/db.ts
+++ lib/redis/db.ts
@@ -1,13 +1,8 @@
 import Redis from 'ioredis'
 import { schedule } from 'node-cron'
-import { REDIS_DB_BASE, REDIS_HOST, REDIS_PASS, REDIS_PORT } from '~env'
+import { REDIS_URL } from '~env'
 
-export const redis = new Redis({
-  db: REDIS_DB_BASE + 0,
-  host: REDIS_HOST,
-  password: REDIS_PASS,
-  port: REDIS_PORT,
-})
+export const redis = new Redis(REDIS_URL)
 
 redis.on('error', () => {
   console.log('Failed to connect to Redis')
